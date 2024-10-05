8bb504df1b8aebad60d4759f928eafa36be0994b
Sat Hatsu <eduardokohatsu21@gmail.com>
2024-10-04 19:12:50 -0500

    Mi primer commit ou iea

---
+++ insert Tb_Mascota.sql
@@ -0,0 +1,19 @@
+INSERT INTO Tb_Mascota (nombre, tipo, departamento_id)
+VALUES 
+('Firulais', 'Perro', 5),
+('Mimi', 'Gato', 12),
+('Bunny', 'Conejo', 3),
+('Rex', 'Reptil', 18),
+('Sasha', 'Perro', 15),
+('Luna', 'Gato', 9),
+('Tweety', 'Pájaro', 1),
+('Spike', 'Reptil', 20),
+('Rocky', 'Perro', 8),
+('Chloe', 'Gato', 2),
+-- No hay mascota en el departamento 4
+('Coco', 'Pájaro', 11),
+-- No hay mascota en el departamento 13
+('Bella', 'Perro', 19),
+('Daisy', 'Pájaro', 10),
+-- No hay mascota en el departamento 17
+('Thumper', 'Conejo', 6);
