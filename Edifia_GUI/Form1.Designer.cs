a191513f2a3eb49c1751da7f841b2f2052c21b17
DavierAL <davierlopez18dl@gmail.com>
2024-10-08 22:17:58 -0500

    Agregando Login y base del proyecto

--- Edifia_GUI/Form1.Designer.cs
+++ Edifia_GUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Edifia_GUI
 {
-    partial class Form1
+    partial class frmHome
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            menuStrip1 = new MenuStrip();
+            ventanaToolStripMenuItem = new ToolStripMenuItem();
+            ventana1ToolStripMenuItem = new ToolStripMenuItem();
+            ventana2ToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { ventanaToolStripMenuItem });
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.Name = "menuStrip1";
+            // 
+            // ventanaToolStripMenuItem
+            // 
+            ventanaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ventana1ToolStripMenuItem, ventana2ToolStripMenuItem });
+            ventanaToolStripMenuItem.Name = "ventanaToolStripMenuItem";
+            resources.ApplyResources(ventanaToolStripMenuItem, "ventanaToolStripMenuItem");
+            // 
+            // ventana1ToolStripMenuItem
+            // 
+            ventana1ToolStripMenuItem.Name = "ventana1ToolStripMenuItem";
+            resources.ApplyResources(ventana1ToolStripMenuItem, "ventana1ToolStripMenuItem");
+            // 
+            // ventana2ToolStripMenuItem
+            // 
+            ventana2ToolStripMenuItem.Name = "ventana2ToolStripMenuItem";
+            resources.ApplyResources(ventana2ToolStripMenuItem, "ventana2ToolStripMenuItem");
+            // 
+            // statusStrip1
+            // 
+            resources.ApplyResources(statusStrip1, "statusStrip1");
+            statusStrip1.Name = "statusStrip1";
+            // 
+            // frmHome
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(46, 52, 73);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "frmHome";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private StatusStrip statusStrip1;
+        private ToolStripMenuItem ventanaToolStripMenuItem;
+        private ToolStripMenuItem ventana1ToolStripMenuItem;
+        private ToolStripMenuItem ventana2ToolStripMenuItem;
     }
 }
